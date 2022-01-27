da160cb27736f4783b46b3b7e2f6b4b02cc96ee2
Srijan945 <sainisrijan95820@gmail.com>
2022-01-27 11:02:39 +0530

    Complete Responsive Social Media Dashboard

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 document.querySelector(".toggle-bar").addEventListener("click",function(){
 
     document.querySelector(".toggle-bar div").classList.toggle("toggle-mode");
+    document.getElementsByTagName("body")[0].classList.toggle("dark");
+    document.getElementsByClassName("toggle-bar")[0].classList.toggle("toggle-bar-color");
+
 });
